bfcdd4bcf2888206f70b4c6175f36edaf1bd1445
John Traner <jatraner@gmail.com>
2020-05-03 15:24:33 -0700

    Added Flask REST API for PUT

---
+++ TrustAPI.py
@@ -0,0 +1,24 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'JWT': u''
+    }
+]
+#Need to impement PUT. The Handler will pass JWT here.
+@app.route('/todo/api/v1.0/tasks', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'JWT' in request.json and type(request.json['JWT']) != unicode:
+        abort(400)
+    
+    tasks[0]['JWT'] = request.json.get('JWT', tasks[0]['JWT'])
+    return jsonify({'tasks': tasks[0]})
