c6ae1942019f5b8aa7622f86d8cb993d4bfa5999
John Traner <jatraner@gmail.com>
2020-05-04 19:54:12 -0700

    REST API now works!

--- TrustAPI.py
+++ TrustAPI.py
@@ -1,16 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
 tasks = [
     {
         'id': 1,
-        'JWT': u'test'
+        'User': u'test'
     }
 ]
 #Need to implement PUT. The Handler will pass JWT here.
-@app.route('/todo/api/v1.0/tasks', methods=['PUT'])
+@app.route('/<int:task_id>', methods=['GET', 'PUT'])
 def update_task(task_id):
+
+    if request.method == 'GET':
+        return jsonify({'tasks': tasks})
+    
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
@@ -21,3 +26,13 @@ def update_task(task_id):
     
     tasks[0]['JWT'] = request.json.get('JWT', tasks[0]['JWT'])
     return jsonify({'tasks': tasks[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#Query Command
+# curl -i -H "Content-Type: application/json" -X PUT -d '{"User":VALUE}' http://localhost:5000/1
