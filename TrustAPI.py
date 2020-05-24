8d9727d05ba4f8cd1ce1c5e242ef69ad1b5697b3
snazzzy <jatraner@gmail.com>
2020-05-24 12:25:27 -0700

    Handler can now query the the TrustAPI.py.

--- TrustAPI.py
+++ TrustAPI.py
@@ -11,28 +11,31 @@ tasks = [
     }
 ]
 
-#Need to implement PUT. The Handler will pass JWT here.
 @app.route('/<int:task_id>', methods=['GET', 'PUT'])
 def update_task(task_id):
 
     if request.method == 'GET':
+
         return jsonify({'tasks': tasks})
-    
+
+
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     if not request.json:
         abort(400)
-    
+
     tasks[0]['value'] = request.json.get('value', tasks[0]['value'])
+    #time.sleep(5)
     return jsonify({'tasks': tasks[0]})
 
+    
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run(host='192.168.1.103', port=5001)
     app.run(debug=True)
 
 #Query Command
